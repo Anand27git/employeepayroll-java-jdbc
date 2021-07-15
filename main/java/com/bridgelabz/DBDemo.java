4584df3e0a1ee5a1f973c4278f29f483c0249ef5
Anand27git <anandkumar256k@gmail.com>
2021-07-15 21:58:52 +0530

    connection done with jdbc-java

---
+++ main/java/com/bridgelabz/DBDemo.java
@@ -0,0 +1,37 @@
+package com.bridgelabz;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+public class DBDemo {
+    public static void main(String[] args) {
+        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false&characterEncoding=utf8";
+        String userName = "root";
+        String password = "root";
+        Connection con;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver loaded!");
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException("cannot find the driver in the classpath", e);
+        }
+        listDrivers();
+        try {
+            System.out.println("connecting to database:" + jdbcURL);
+            con = DriverManager.getConnection(jdbcURL, userName, password);
+            System.out.println("connection is successful!!!" + con);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void listDrivers() {
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while (driverList.hasMoreElements()) {
+            Driver driverClass = (Driver) driverList.nextElement();
+            System.out.println("" + driverClass.getClass().getName());
+        }
+    }
+}
